ba74eb10eff6aa5703f198c43bb2dfb2c4fdb8bf
LifelongLearner13 <sierragregg@gmail.com>
2016-11-01 18:12:14 -0500

    code as provided in tutorial needs a lot of modifications

--- main.cpp
+++ main.cpp
@@ -1,6 +1,140 @@
-#include <iostream>
+#include <cstdlib>
+#include <SFML/Graphics.hpp>
+#include "paddle.h"
+#include "ball.h"
+#include <sstream>
+
 
 int main(int argc, const char * argv[])
 {
+    int windowWidth = 1024;
+    int windowHeight = 768;
+    // Make a window that is 1024 by 768 pixels
+    // And has the title "Pong"
+    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Pong");
+    
+    int score = 0;
+    int lives = 3;
+    
+    // create a bat
+    Paddle paddle (windowWidth / 2, windowHeight - 20, .3f);
+    
+    // create a ball
+    Ball ball(windowWidth / 2, 1);
+    
+    // Create a "Text" object called "message". Weird but we will learn about objects soon
+    sf::Text hud;
+    
+    // We need to choose a font
+    sf::Font font;
 
+    font.loadFromFile("Summerti.ttf");
+    
+    // Set the font to our message
+    hud.setFont(font);
+    
+    // Make it really big
+    hud.setCharacterSize(75);
+    
+    // Choose a color
+    hud.setFillColor(sf::Color::White);
+    
+    while (window.isOpen())
+    {
+        // Handle the player input
+        
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                // Someone closed the window- bye
+                window.close();
+        }
+        
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        {
+            // move left...
+            //paddle.moveLeft();
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        {
+            // move right...
+            //paddle.moveRight();
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+        {
+            // quit...
+            // Someone closed the window- bye
+            window.close();
+        }
+    
+        //Update the frame
+    
+        // Handle ball hitting the bottom
+        if (ball.getPosition().top > windowHeight)
+        {
+            // reverse the ball direction
+            ball.hitBottom();
+        
+            // Remove a life
+            lives --;
+        
+            // Check for zero lives
+            if (lives < 1) {
+                // reset the score
+                score = 0;
+                // reset the lives
+                lives = 3;
+            }
+        
+        }
+    
+        // Handle ball hitting top
+        if (ball.getPosition().top < 0)
+        {
+            ball.reboundBatOrTop();
+        
+            // Add a point to the players score
+            score++;
+        
+        }
+    
+        // Handle ball hitting sides
+        if (ball.getPosition().left < 0 || ball.getPosition().left + 10 > windowWidth)
+        {
+            ball.reboundSides();
+        }
+    
+        // Has the ball hit the bat?
+        if (ball.getPosition().intersects(paddle.getPosition()))
+        {
+            // Hit detected so reverse the ball and score a point
+            ball.reboundBatOrTop();
+        }
+        
+        ball.update();
+        paddle.update();
+        
+        // Update the HUD text
+        std::stringstream ss;
+        ss << "Score:" << score << "    Lives:" << lives;
+        hud.setString(ss.str());
+
+        // Draw the frame
+        
+        // Clear everything from the last frame
+        window.clear(sf::Color(26, 128, 182,255));
+        
+        window.draw(paddle.getShape());
+        
+        window.draw(ball.getShape());
+        
+        // Draw our score
+        window.draw(hud);
+        
+        // Show everything we just drew
+        window.display();
+    }// This is the end of the "while" loop
+    
+    return 0;
 }
